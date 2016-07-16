c346e0746503e31169993f0cfeb6312c3f2461ed
atenklsy <atenk@qq.com>
2016-07-16 23:40:16 +0800

    修改showDetail

--- app/src/main/java/com/mengshitech/colorrun/fragment/show/showFragment.java
+++ app/src/main/java/com/mengshitech/colorrun/fragment/show/showFragment.java
@@ -49,7 +49,7 @@ public class showFragment extends Fragment {
      */
     private void initDatas() {
         FragmentManager fm = getActivity().getSupportFragmentManager();
-        lvShowContent.setAdapter(new ShowAdapter(mActivity, mShowList, lvShowContent,fm));
+        lvShowContent.setAdapter(new ShowAdapter(mActivity, mShowList, lvShowContent, fm));
     }
 
     /**
@@ -66,6 +66,26 @@ public class showFragment extends Fragment {
         mShowEntity1.setTvShow_Heart("129");
         mShowEntity1.setTvShow_Comment("221");
         mShowList.add(mShowEntity1);
+
+        ShowEntity mShowEntity2 = new ShowEntity();
+        mShowEntity2.setIvUserHead(R.mipmap.image_head);
+        mShowEntity2.setTvShow_UserName("陈永银");
+        mShowEntity2.setIvShowPic(R.mipmap.historybackground);
+        mShowEntity2.setTvWordContent("#爱上乐跑#本期跑者Vinglu，他完成了“为爱同行”2016南昌乐跑公益活动，感慨道，“跑了就懂，只要坚持，蜗牛也是牛！”。");
+        mShowEntity2.setTvSendTime("2016年7月12日11:25");
+        mShowEntity2.setTvShow_Heart("12");
+        mShowEntity2.setTvShow_Comment("61");
+        mShowList.add(mShowEntity2);
+
+        ShowEntity mShowEntity3 = new ShowEntity();
+        mShowEntity3.setIvUserHead(R.mipmap.image_head);
+        mShowEntity3.setTvShow_UserName("康惠聪");
+        mShowEntity3.setIvShowPic(R.mipmap.historybackground);
+        mShowEntity3.setTvWordContent("#爱上乐跑#本期跑者Vinglu，他完成了“为爱同行”2016南昌乐跑公益活动，感慨道，“跑了就懂，只要坚持，蜗牛也是牛！”。");
+        mShowEntity3.setTvSendTime("2016年7月13日09:25");
+        mShowEntity3.setTvShow_Heart("29");
+        mShowEntity3.setTvShow_Comment("113");
+        mShowList.add(mShowEntity3);
 
     }
 }
