a8a7a1c2d37b2679f911c426c9b2a99872b6836f
atenklsy <atenk@qq.com>
2016-07-19 16:45:41 +0800

    提交部分

--- app/src/main/java/com/mengshitech/colorrun/fragment/lerun/LerunEventListView.java
+++ app/src/main/java/com/mengshitech/colorrun/fragment/lerun/LerunEventListView.java
@@ -4,47 +4,67 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import com.mengshitech.colorrun.R;
 import com.mengshitech.colorrun.adapter.LeRunEventListviewAdapter;
 import com.mengshitech.colorrun.fragment.BaseFragment;
+import com.mengshitech.colorrun.utils.Utility;
 
 /**
  * Created by kanghuicong on 2016/7/15  16:12.
  * 515849594@qq.com
  */
-public class LerunEventListView extends BaseFragment {
+public class LerunEventListView extends BaseFragment implements View.OnClickListener{
     ListView lerun_listView;
     LeRunEventListviewAdapter adapter;
     View lerunevent;
+    TextView title_bar;
+    ImageView title_back;
 
     @Override
     public View initView() {
-        lerunevent = View.inflate(getActivity(), R.layout.lerun_event, null);
+        lerunevent = View.inflate(getActivity(), R.layout.lerun_event,null);
         find();
-        adapter = new LeRunEventListviewAdapter(getActivity());
-        lerun_listView.setAdapter(adapter);
-
-        lerun_listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view,
-                                    int position, long id) {
-                toFragment(new IntoLerunEvent());
-            }
-        });
-
+        startAdapter();//listview的adapter
+        click();
         return lerunevent;
     }
 
-    private void find() {
-        lerun_listView = (ListView) lerunevent.findViewById(R.id.lerun_listView);
+    private void click() {
+        title_back.setOnClickListener(this);
     }
 
-    private void toFragment(Fragment fragment) {
-        FragmentManager fmManager = getFragmentManager();
-        fmManager.beginTransaction().replace(R.id.flMain, fragment)
-                .addToBackStack("fragment").commit();
+    private void startAdapter() {
+        adapter = new LeRunEventListviewAdapter(getActivity());
+        lerun_listView.setAdapter(adapter);
+
+        //listview  item点击事件
+        lerun_listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                FragmentManager fragmentManager = getFragmentManager();
+                Utility.replace2DetailFragment(fragmentManager,new IntoLerunEvent());
+            }
+        });
+    }
+
+    private void find() {
+        lerun_listView = (ListView)lerunevent.findViewById(R.id.lerun_listView);
+        title_bar = (TextView)lerunevent.findViewById(R.id.title_barr);
+        title_bar.setText("活动");
+        title_back = (ImageView)lerunevent.findViewById(R.id.title_back);
+    }
+
+    //点击事件
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.title_back:
+                getFragmentManager().popBackStack();
+        }
     }
 }
--- app/src/main/java/com/mengshitech/colorrun/fragment/me/myDetailFragment.java
+++ app/src/main/java/com/mengshitech/colorrun/fragment/me/myDetailFragment.java
@@ -1,12 +1,14 @@
 package com.mengshitech.colorrun.fragment.me;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.mengshitech.colorrun.MainActivity;
 import com.mengshitech.colorrun.R;
@@ -15,16 +17,62 @@ import com.mengshitech.colorrun.fragment.BaseFragment;
 /**
  * atenklsy
  */
-public class myDetailFragment extends BaseFragment {
+public class myDetailFragment extends BaseFragment implements View.OnClickListener{
     View mDeatilView;
+    LinearLayout me_head,me_nickname,me_phone,me_address,me_sex,me_land,me_autograph;
 
 
     @Override
     public View initView() {
         mActivity = getActivity();
         mDeatilView = View.inflate(mActivity, R.layout.fragment_mydetail, null);
+        find();
+        click();
         return mDeatilView;
     }
 
+    private void click() {
+        me_sex.setOnClickListener(this);
+    }
 
+    private void find() {
+        me_head = (LinearLayout)mDeatilView.findViewById(R.id.ll_me_head);
+        me_nickname = (LinearLayout)mDeatilView.findViewById(R.id.ll_me_nickname);
+        me_phone = (LinearLayout)mDeatilView.findViewById(R.id.ll_me_phone);
+        me_address = (LinearLayout)mDeatilView.findViewById(R.id.ll_me_address);
+        me_sex = (LinearLayout)mDeatilView.findViewById(R.id.ll_me_sex);
+        me_land = (LinearLayout)mDeatilView.findViewById(R.id.ll_me_land);
+        me_autograph = (LinearLayout)mDeatilView.findViewById(R.id.ll_me_autograph);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.ll_me_head:
+
+                break;
+            case R.id.ll_me_nickname:
+
+                break;
+            case R.id.ll_me_phone:
+
+                break;
+            case R.id.ll_me_address:
+
+                break;
+            case R.id.ll_me_sex:
+                
+                break;
+            case R.id.ll_me_land:
+
+                break;
+            case R.id.ll_me_autograph:
+
+                break;
+
+            default:
+                break;
+        }
+    }
 }
